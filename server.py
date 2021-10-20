41346e7242ff8fb1aba87ea048b4bc4a5fe89721
Ivan <60302361+depocoder@users.noreply.github.com>
2021-10-20 20:40:20 +0300

    add response from socket

--- server.py
+++ server.py
@@ -1,12 +1,44 @@
 import socket
+import logging
 
 
-server = socket.create_server(('127.0.0.1', 8000))
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
-server.listen(10)
 
-client_socket, address = server.accept()
-received_date = client_socket.recv(1024).decode('utf-8 ')
+def close_socket():
+    try:
+        #  don't work on my macbook
+        server.shutdown(socket.SHUT_RDWR)
+    except OSError:
+        pass
+    server.close()
+    logger.info('Server was closed')
 
-print(f'Got data by socket {received_date}')
+
+if __name__ == '__main__':
+    with open('index.html') as file:
+        html = file.read()
+
+    server = socket.create_server(('127.0.0.1', 8001))
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server.listen(10)
+
+
+    while True:
+        try:
+            client_socket, address = server.accept()
+        except KeyboardInterrupt: # when user presed ctr + c
+            close_socket()
+            break
+        received_date = client_socket.recv(1024).decode('utf-8 ')
+
+        logger.info(f'Got data by socket {received_date}')
+        path = received_date.split()[1]
+
+        logger.info(f"Parsed {path=}")
+
+        response = f'HTTP/1.1 200 OK\nContent-type: text/html; charset=utf-8\n\n{html}'
+        client_socket.send(response.encode('utf-8'))
+        client_socket.shutdown(socket.SHUT_RDWR)
